17574b2849fd1b1e612cdab8fe2650bef5730b45
EC2 Default User <ec2-user@ip-172-31-22-55.us-west-2.compute.internal>
2020-08-08 14:39:45 +0000

    Add all my files

---
+++ create_infra.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name ContainerHost --template-body file://capstone_infra.yml  --parameters file://capstone_infra_parameter.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
