6cf24b5b6097e92e521b39543ba0284f1addefe7
yara-selim <67265189+yara-selim@users.noreply.github.com>
2020-08-09 14:22:14 +0200

    Update run-k8s.sh

--- run-k8s.sh
+++ run-k8s.sh
@@ -4,12 +4,13 @@
 dockerpath=yaraselim1994/cloud-devops-capstone
 
 # Run the Docker Hub container with kubernetes
-kubectl run capstone --image=$dockerpath --port=80
+kubectl run capstone\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 
 
 # List kubernetes pods
-sleep 10
 kubectl get pods
 
 # Forward the container port to a host
-POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-kubectl port-forward $POD_NAME 80:80
+kubectl port-forward capstone 8000:80
