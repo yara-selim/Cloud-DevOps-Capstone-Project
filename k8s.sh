71a67a8dfdf2beff89596206c40314706a485cf0
yara-selim <67265189+yara-selim@users.noreply.github.com>
2020-08-11 14:58:45 +0200

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,5 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=yaraselim1994/cloud-devops-capstone
+
+# Step 2
+# Run the Docker Hub container with kubernetes
 kubectl run capstone\
---image=yaraselim1994/cloud-devops-capstone\
---port=80
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
 kubectl port-forward --address 0.0.0.0 capstone 8888:80
